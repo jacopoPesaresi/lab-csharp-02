546ce93ec0c3aee66d46463e348b88636c457da8
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2022-12-13 14:51:24 +0100

    Exercise 1 done. Waiting for corrections

--- 1-Collections/SocialNetworkUser.cs
+++ 1-Collections/SocialNetworkUser.cs
@@ -6,28 +6,46 @@ namespace Collections
     public class SocialNetworkUser<TUser> : User, ISocialNetworkUser<TUser>
         where TUser : IUser
     {
-        ISet<User> followers = new HashSet<User>();
+        IDictionary<string, ISet<TUser>> social = new Dictionary<string, ISet<TUser>>();
+        //ISet<User> followers = new HashSet<User>();
+        IUser myUser;
         public SocialNetworkUser(string fullName, string username, uint? age) : base(fullName, username, age)
         {
+            myUser = new User(fullName, username, age);
             //throw new NotImplementedException("TODO is there anything to do here?");
         }
 
         public bool AddFollowedUser(string group, TUser user)
         {
-            throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
+            if (!social.Keys.Contains(group)){
+                social[group] = new HashSet<TUser>();
+            }
+            return social[group].Add(user);
+            //throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
         }
 
         public IList<TUser> FollowedUsers
         {
             get
             {
-                throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                IList<TUser> tmp = new List<TUser>();
+                foreach (var item in social)
+                {
+                    foreach (TUser us in item.Value)
+                    {
+                        tmp.Add(us);
+                    }
+                }
+                return tmp;
+                //throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
             }
         }
 
         public ICollection<TUser> GetFollowedUsersInGroup(string group)
         {
-            throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
+            if (!social.Keys.Contains(group)) return new HashSet<TUser>();
+            return social[group];
+            //throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
         }
     }
 }
